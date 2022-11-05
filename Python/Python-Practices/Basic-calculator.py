adb09e1010e597333736718b2d310ce54f0480e3
Xiaotong <111238960+Xjungd116@users.noreply.github.com>
2022-11-05 17:33:41 +0000

    Create Basic-calculator.py

---
+++ Python/Python-Practices/Basic-calculator.py
@@ -0,0 +1,17 @@
+# case 1: python automatically convert input into strings
+number_1 = input("Enter a number: ")
+number_2 = input("Enter another number: ")
+result = number_1 + number_2
+print(result)
+
+# case 2: convert strings into whole number with int()
+number_1 = input("Enter a number: ")
+number_2 = input("Enter another number: ")
+result = int(number_1) + int(number_2)
+print(result)
+
+# case 2: convert strings into number with float()
+number_1 = input("Enter a number: ")
+number_2 = input("Enter another number: ")
+result = float(number_1) + float(number_2)
+print(result)
