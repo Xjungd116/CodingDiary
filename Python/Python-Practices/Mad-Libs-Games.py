b49bf3e2837fe93297975fa72164c6c4fac7b3f7
Xiaotong <111238960+Xjungd116@users.noreply.github.com>
2022-11-05 17:34:11 +0000

    Create Mad-Libs-Games.py

---
+++ Python/Python-Practices/Mad-Libs-Games.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plunoun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plunoun + " are blue")
+print("I love " + celebrity)
